163f113a4124080b308d4c501f60a1ad4f788394
NHQ <jonathon@nationalheadquarters.org>
2011-06-26 14:35:58 -0700

    sunday lord

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.bodyParser());
+  //app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }));
   app.use(express.session({ store: new RedisStore, secret: 'keyboard cat' }));
